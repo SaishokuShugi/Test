ec711e71f1c22ecc85a92f833a0df2ec47532958
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2018-01-25 22:27:11 +0100

    lol

---
+++ TestJava/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public main() {
+		// TODO Stub du constructeur généré automatiquement
+	}
+
+	public static void main(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+
+	}
+
+}
